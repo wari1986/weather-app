7f7bfa850b3d85ba056f85147b983804af83ecb2
wari <nicolaycamacho@gmail.com>
2020-09-02 01:46:13 -0400

    error: false added to setState

--- src/App.js
+++ src/App.js
@@ -93,6 +93,7 @@ class App extends React.Component {
         temp_max:this.calCelsius(response.main.temp_max),
         temp_min:this.calCelsius(response.main.temp_min),
         description:response.weather[0].description,
+        error:false
       });
 
       this.getWeatherIcon(this.weatherIcon,response.weather[0].id);
